133bcaad453a653b7701e301066dd7778c02a03b
delboy96 <danilo.milosevic.36.16@ict.edu.rs>
2023-06-11 18:20:49 +0200

    Task 5 halfway there

--- php/queries.php
+++ php/queries.php
@@ -2,7 +2,35 @@
 
 function oneB(PDO $conn): ?array
 {
-    $sql = "SELECT u.firstname, u.lastname, o.id, o.value as ukupnaVrednost FROM user u INNER JOIN order o ON u.id = o.userId";
+    $sql = "SELECT u.firstname, u.lastname, o.id, SUM(o.value) as ukupnaVrednost FROM user u INNER JOIN order o ON u.id = o.userId";
+    $stmt = $conn->prepare($sql);
+    try{
+        $stmt->execute();
+        $data = $stmt -> fetchAll();
+        return ($stmt->rowCount() > 0) ? $data : null;
+    }catch(PDOException $e){
+        // Log error ($e->getMessage())
+        return null;
+    }
+}
+
+function twoC(PDO $conn): ?array
+{
+    $sql = "SELECT u.*, COUNT(o.id) as brojPorudzbina FROM `user` u INNER JOIN `order` o ON u.id = o.userId GROUP BY u.id HAVING COUNT(o.id) > 1";
+    $stmt = $conn->prepare($sql);
+    try{
+        $stmt->execute();
+        $data = $stmt -> fetchAll();
+        return ($stmt->rowCount() > 0) ? $data : null;
+    }catch(PDOException $e){
+        // Log error ($e->getMessage())
+        return null;
+    }
+}
+
+function twoD(PDO $conn): ?array
+{
+    $sql = "SELECT u.firstname, u.lastname, o.id, COUNT(oi.id) as brojStavkiPorudzbine FROM `user` u INNER JOIN `order` o ON u.id = o.userId INNER JOIN `orderitem` oi ON o.id = oi.orderId GROUP BY o.id";
     $stmt = $conn->prepare($sql);
     try{
         $stmt->execute();
